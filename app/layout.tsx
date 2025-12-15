8af131c515b5b1926d7c8c03c57ba7de66e99868
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-12-15 20:43:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navigation/navbar";
 import { ReactLenis, useLenis } from "lenis/react";
-import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -57,7 +56,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {/* <ReactLenis root /> */}
-          <Analytics/>
           <Navbar />
           {children}
         </ThemeProvider>
