c8ad080c1b8afab2c5231cc6f2ea23ca829bf2fe
Arnab Mukherjee <136512754+arnab-git-404@users.noreply.github.com>
2025-12-15 20:22:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <ReactLenis root />
+          {/* <ReactLenis root /> */}
           <Analytics/>
           <Navbar />
           {children}
